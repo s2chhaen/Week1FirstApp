820082840f69c3bfd190b201d896341131e4a39d
Christian Haendel <example@example.com>
2023-12-13 21:07:17 +0200

    add my first app

---
+++ Week1FirstApp/Program.cs
@@ -0,0 +1,9 @@
+﻿// This is a comment
+
+string firstname = "Christian";
+string lastname = "Händel";
+
+//Console.WriteLine("Hello" + " " + "firstname!");
+
+Console.WriteLine($"Hello {firstname} {lastname}!");
+
